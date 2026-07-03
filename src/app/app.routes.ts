b66b971fa0517c717ec40d9033681f73eb26080a
Fane Yaya <y.fane75@hotmail.fr>
2026-07-04 01:58:52 +0200

    maj -1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Actions } from './pages/actions/actions';
+import { Partners } from './pages/partners/partners';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'qui-sommes-nous', component: About },
+  { path: 'actions', component: Actions },
+  { path: 'partenaires', component: Partners },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' }
+];
