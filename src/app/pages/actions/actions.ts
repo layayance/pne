3a5041c8585c0cefe6b443c81c4138adc7280bf4
Fane Yaya <y.fane75@hotmail.fr>
2026-07-03 22:30:19 +0200

    Creation de page

---
+++ src/app/pages/actions/actions.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actions',
+  imports: [],
+  templateUrl: './actions.html',
+  styleUrl: './actions.scss',
+})
+export class Actions {}
