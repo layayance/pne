da2631039336daa288a4be91c47a7988631782a6
Fane Yaya <y.fane75@hotmail.fr>
2026-07-04 02:37:11 +0200

    Build PNE static website pages

--- src/app/pages/actions/actions.ts
+++ src/app/pages/actions/actions.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-actions',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './actions.html',
   styleUrl: './actions.scss',
 })
--- src/app/pages/partners/partners.ts
+++ src/app/pages/partners/partners.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-partners',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './partners.html',
   styleUrl: './partners.scss',
 })
