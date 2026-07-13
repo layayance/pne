97549ef0eb5f0178556a0e2a60cfaa05a91d55bd
Fane Yaya <y.fane75@hotmail.fr>
2026-07-13 12:46:13 +0200

    Ajout Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { inject } from '@vercel/analytics';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    inject();
+  })
+  .catch(err => console.error(err)); 
